807e3e8a5bb9089e39c3c1f3060dcefb4c464c01
wangdong <dong124012@163.com>
2021-11-12 17:51:16 +0800

    add vue-ls

--- src/main.js
+++ src/main.js
@@ -7,14 +7,25 @@ import localData from './componentoperation/local'
 import A from './until/api'
 import './registerServiceWorker'
 import ElementUI from 'element-ui';
+import Storage from 'vue-ls';
+ 
+
 import 'element-ui/lib/theme-chalk/index.css';
 import 'normalize.css/normalize.css'
 // import '../utils/rem.js'
 Vue.use(ElementUI);
 
+
 Vue.config.productionTip = false
 Vue.prototype.$local = localData
 Vue.prototype.$api = A
+const options = {
+  namespace: '', // key键前缀
+  name: 'ls', // 命名Vue变量.[ls]或this.[$ls],
+  storage: 'local', // 存储名称: session, local, memory
+};
+ 
+Vue.use(Storage, options);
 new Vue({
   router,
   store,
